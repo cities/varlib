30b29d0f0cc519c04c967148d0969d432a064f6b
lmwang <lmwang@gmail.com>
2019-11-25 20:52:24 -0800

    work-in-progress; use nx to handle dep graph

---
+++ ast_dependencies.py
@@ -0,0 +1,103 @@
+dep_graph = {}
+
+import yaml
+import pandas as pd
+
+with open(r'variables.yml') as file:
+    # The FullLoader parameter handles the conversion from YAML
+    # scalar values to Python the dictionary format
+    variables = yaml.load(file, Loader=yaml.FullLoader)
+
+    #print(variables)
+
+ops = [
+    'aggregate',
+    'disaggregate',
+]    
+
+import ast
+from pprint import pprint
+
+
+class Analyzer(ast.NodeVisitor):
+    def __init__(self):
+        self.deps = []
+        self.crosswalk = {"deps":[]}
+        self.funcs = []
+
+    def visit_Assign(self, node):
+        lhs, = node.targets
+        rhs = node.value
+        self.generic_visit(rhs)
+
+    def visit_Attribute(self, node):
+        dataset = node.value.id
+        if node.attr == "aggregate":
+            #self.deps += [f"{node.value.id}.{node.value.id}_id"]
+            self.crosswalk["tgt_id"] = f"{dataset}_id"
+        elif node.attr == "disaggregate":
+            #self.deps += [f"{node.value.id}.{node.value.id}_id"]
+            self.crosswalk["src_ds"] = f"{dataset}"
+        else:
+            self.deps += [f"{dataset}.{node.attr}"]
+            if "tgt_id" in self.crosswalk:
+                self.crosswalk["deps"] += [f"{dataset}.{self.crosswalk['tgt_id']}"]
+            elif "src_ds" in self.crosswalk:
+                self.crosswalk["deps"] += [f"{self.crosswalk['src_ds']}.{dataset}_id"]
+                
+    def visit_Call(self, node):
+        #if type(node.func) is ast.Name:
+        #    self.generic_visit(node.args)
+        #if type(node.func) is ast.Attribute:
+        #    self.deps += [f"{node.func.value.id}.{node.func.value.id}_id"]
+        self.funcs += [node.func]
+        [self.visit(arg) for arg in node.args]
+        [self.visit(kwarg) for kwarg in node.keywords]
+    
+    def visit_Name(self, node):
+        if hasattr(node, "attr"):
+            self.deps += [f"{node.id}.{node.attr}"]
+        else:
+            self.deps += [node.id]
+    
+    def report(self):
+        pprint(self.deps)
+
+   
+# how can I know a ast.Name is a DataFrame column?
+import ast
+for df_name, var_exprs in variables.items():
+    for expr in var_exprs:
+        #print(expr)
+        module = ast.parse(expr)
+        stmt = module.body[0]
+        lhs, = stmt.targets
+        analyzer = Analyzer()
+        analyzer.visit(stmt)
+        analyzer.report()
+        dep_graph[f"{df_name}.{lhs.id}"] = [f"{dep}" if "." in dep 
+                                              else f"{df_name}.{dep}" 
+                                            for dep in analyzer.deps + 
+                                               analyzer.crosswalk.get("deps")
+                                           ]
+
+        
+# tests
+def compare_dict(dict1, dict2):
+    for x1 in dict1.keys():
+        z = dict1.get(x1) == dict2.get(x1)
+        if not z:
+            print('key', x1)
+            print('Actual', dict1.get(x1), '\nExpected', dict2.get(x1))
+            print('-----\n')
+            
+expected_graph = {'household.nadults': ['household.hhsize', 'household.nchildren'],
+ 'household.cars_per_adults': ['household.cars', 'household.nadults'],
+ 'household.cars_per_adults_gt1': ['household.cars_per_adults'],
+ 'household.sqrt_hhsize': ['household.hhsize'],
+ 'household.nchildren': ['person.is_child', 'person.household_id'],
+ 'person.is_child': ['person.age'],
+ 'person.nadults': ['household.nadults', 'person.household_id'] 
+                 }
+
+compare_dict(dep_graph, expected_graph)
--- process_dependencies.py
+++ process_dependencies.py
@@ -1,6 +1,7 @@
 dep_graph = {}
 
 import yaml
+import pandas as pd
 
 with open(r'variables.yml') as file:
     # The FullLoader parameter handles the conversion from YAML
@@ -9,6 +10,11 @@ with open(r'variables.yml') as file:
 
     #print(variables)
 
+ops = [
+    'aggregate',
+    'disaggregate',
+]    
+    
 # how can I know a ast.Name is a DataFrame column?
 import ast
 for df_name, var_exprs in variables.items():
@@ -22,18 +28,58 @@ for df_name, var_exprs in variables.items():
         rhs = stmt.value
         #print(lhs, rhs)
         rhs_names = set([node.id for node in ast.walk(rhs)
-                     if type(node) is ast.Name])
+                         if type(node) is ast.Name])
         #remove function calls (may want to keep tracking them)
         func_names = set([node.func.id for node in ast.walk(rhs)
-                     if type(node) is ast.Call and type(node.func) is ast.Name])
+                         if type(node) is ast.Call and 
+                              type(node.func) is ast.Name])
         #handle pd DataFrame methods
-        
-        rhs_names = [name if "." in name else f"{df_name}.{name}" 
-                     for name in rhs_names - func_names]    
-        dep_graph[f"{df_name}.{lhs.id}"] = rhs_names
+        method_names = set([node.func.attr for node in ast.walk(rhs)
+                            if type(node) is ast.Call and 
+                              type(node.func) is ast.Attribute])
+        if type(rhs) is ast.Call and type(rhs.func) is ast.Attribute:
+            var_names = [f"{rhs.func.value.id}.{rhs.func.value.id}_id"]
+            var_names += [f"{arg.value.id}.{arg.attr}" for arg in rhs.args]
+        else:
+            var_names = [name if "." in name else f"{df_name}.{name}" 
+                         for name in rhs_names - func_names]
+        dep_graph[f"{df_name}.{lhs.id}"] = var_names
         #print(names)
 
 
+import pandas as pd
+import numpy as np
+from pandas.core.computation.expr import Expr
+from pandas.core.computation.scope import Scope
+
+code = "sqrt_nch = sqrt(nchildren + 1)"
+env = Scope(1, local_dict={"hhsize":3, "nchildren":2}, target="c")
+aa = Expr(code, env=env)
+
+data = {'person_id': [1, 2, 3, 4],
+        'household_id': [1, 1, 2, 3],
+        'age':[2, 26, 19, 18]} 
+# Create DataFrame 
+person = pd.DataFrame(data) 
+
+person = person.assign(is_child=lambda x: (x["age"]<18).astype(int))
+#pd.eval("is_child=np.where(person.age<18, 1, 0)", target=person)
+#person.eval("is_child=np.where(age<18, 1, 0)", inplace=True, np=np)
+
+data = {'household_id': [1, 2, 3]} 
+# Create DataFrame 
+household = pd.DataFrame(data).set_index("household_id")
+
+def nchildren(household, person):
+    res = person.groupby("household_id").agg({"is_child":"sum"}).rename(columns={"is_child":"nchildren"})
+    return household.join(res)["nchildren"].values
+
+local_dict={"person":person_df, "np":np}
+code = 'nchildren = person.groupby("household_id").agg(np.sum)'
+env = Scope(1, local_dict=local_dict, target="c")
+#pd.eval(code, local_dict=local_dict, target="c")
+#aa = Expr(code, env=env)
+        
 # tests
 def compare_dict(dict1, dict2):
     for x1 in dict1.keys():
@@ -47,7 +93,7 @@ expected_graph = {'household.nadults': ['household.hhsize', 'household.nchildren
  'household.cars_per_adults': ['household.nadults', 'household.cars'],
  'household.cars_per_adults_gt1': ['household.cars_per_adults'],
  'household.sqrt_hhsize': ['household.hhsize'],
- 'household.nchildren': ['person.household_id', 'person.is_child'],
+ 'household.nchildren': ['household.household_id', 'person.is_child'],
  'person.is_child': ['person.age']
                  }
 
