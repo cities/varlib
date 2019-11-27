f3fa1a9ae63791b689741172a8e5c7cfe391f5d7
lmwang <lmwang@gmail.com>
2019-11-26 20:38:10 -0800

    moved out TODOs into TODO.md; added more TODO items

---
+++ TODO.md
@@ -0,0 +1,20 @@
+# TODO
+- [x] parse variable definitions to find dependency graph for variables
+- [x] recursively compute its dependencies before a variable is evalualted (computed)
+- [ ] refactor varlib into a python package
+- [ ] automate tests
+- [ ] handle version of dependencies, only re-compute when a variable is stale
+   - insert hash digest as 'version' attr in the dep graph for a variable is computed
+   - insert hash digest of dependencies variables into the dependency graph at computing if a column of the name of the variable exists and its version attr in dep graph is not set
+   - re-compute a variable only when 'version' attr for predecessors is not set or differ from current value of hash digest
+- [ ] a simpler parser for parsing common expressions to be evaluated, e.g.
+   * person.eval("@to_numeric(age<18)")
+   * person.eval("(age<18).astype('int')")
+   * pd.eval("(person.age).astype('int')")
+   - [ ] find references of data frame columns in an expression and replace them with Series from the resolvers
+   - [ ] does this approach disable the numexpr engine?
+   - [ ] how much advantage does the numexpr engine provide?
+- [ ] are there other common patterns of variable definitions that need to support?
+- [ ] is there a need to specify dataset relationship? If yes, what is the most sensible way to do so?
+- [ ] get feedback from Paul and team
+- [ ] a variable checker that verifies variable definitions
--- use_pandas.py
+++ use_pandas.py
@@ -1,26 +1,33 @@
 from yaml import load, dump
 variables = load("variables.yml")
 
-#TODO
-#- [ ] parse variable definitions to find dependency graph between variables
-#- [ ] auto insert its dependencies when a variable is evalualted
-#- [ ] handle version of dependencies, only re-compute when a variable is stale
-#- [ ]
-
 import pandas as pd
+
+def aggregate(attr, tgt_df, func="sum", resolvers=None):
+    df1_id = f"{tgt_df.name}_id"
+    df2_name, vname = attr.split(".")
+    df2 = resolvers["df2_name"]
+    res = df2.group_by(df1_id).agg({vname:func})
+    return tgt_df.join(res[vname]).values
+
+def disaggregate(attr, tgt_df, resolvers=None):
+    df1_id = f"{tgt_df.name}_id"
+    df2_name, vname = attr.split(".")
+    df2 = resolvers["df2_name"]
+    return tgt_df.join(df2[vname]).values
 
 class DataFrame(pd.DataFrame):
     def __init__(self, args, kwargs):
         super(self).__init__(args, **kwargs)
 
-    def aggregate(self, attr, func="sum", resolver=None):
+    def aggregate(self, attr, func="sum", tgt_df = "", resolvers=None):
         df1_id = f"{self.name}_id"
         df2_name, vname = attr.split(".")
         df2 = resolver["df2_name"]
         res = df2.group_by(df1_id).agg({vname:func})
         return self.join(res[vname]).values
 
-    def disaggregate(self, attr, resolver=None):
+    def disaggregate(self, attr, tgt_df = "", resolvers=None):
         df1_id = f"{self.name}_id"
         df2_name, vname = attr.split(".")
         df2 = resolver["df2_name"]
