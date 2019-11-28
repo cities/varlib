f6bb7b6c95089db51c598cc244a7475c9b5da757
lmwang <lmwang@gmail.com>
2019-11-27 20:01:55 -0800

    refactored depedency analyzer; moved test to tests/

--- tests/test_mod.py
+++ tests/test_mod.py
@@ -1,5 +1,29 @@
 import varlib
 
+# tests
+def compare_dict(dict1, dict2, verbose=True):
+    for x1 in dict1.keys():
+        z = dict1.get(x1) == dict2.get(x1)
+        assert z
+        if not z and verbose:
+            print('key', x1)
+            print('Actual', dict1.get(x1), '\nExpected', dict2.get(x1))
+            print('-----\n')
 
-def test_has_legs():
-    assert not varlib.has_legs
+
+#compare_dict(resulted_dict, expected_dict)
+def test_parse_deps():
+    expected_dict = {'household.nadults': ['household.hhsize', 'household.nchildren'],
+        'household.cars_per_adults': ['household.cars', 'household.nadults'],
+        'household.cars_per_adults_gt1': ['household.cars_per_adults'],
+        'household.sqrt_hhsize': ['household.hhsize'],
+        'household.nchildren': ['person.is_child', 'person.household_id'],
+        'person.is_child': ['person.age'],
+        'person.is_child_int': ['person.age'],
+        'person.nadults': ['household.nadults', 'person.household_id'],
+        'person.is_girl': ['person.is_child', 'person.sex']
+        }
+
+    resulted_dict, _ = varlib.analyze_dependency("example/variables.yml")
+    compare_dict(expected_dict, resulted_dict, verbose=False)
+
--- varlib/ast_dependencies.py
+++ varlib/ast_dependencies.py
@@ -1,19 +1,9 @@
-dep_graph = {}
-defs = {}
-
 import yaml
 import ast
 import pandas as pd
 from pprint import pprint
 
-with open(r'variables.yml') as file:
-    # The FullLoader parameter handles the conversion from YAML
-    # scalar values to Python the dictionary format
-    variables = yaml.load(file, Loader=yaml.FullLoader)
-
-    #print(variables)
-
-class Analyzer(ast.NodeVisitor):
+class DepAnalyzer(ast.NodeVisitor):
     def __init__(self):
         self.deps = []
         self.crosswalk = {"deps":[]}
@@ -43,7 +33,7 @@ class Analyzer(ast.NodeVisitor):
                 self.crosswalk["deps"] += [f"{dataset}.{self.crosswalk['tgt_id']}"]
             elif "src_ds" in self.crosswalk:
                 self.crosswalk["deps"] += [f"{self.crosswalk['src_ds']}.{dataset}_id"]
-                
+
     def visit_Call(self, node):
         #if type(node.func) is ast.Name:
         #    self.generic_visit(node.args)
@@ -52,54 +42,44 @@ class Analyzer(ast.NodeVisitor):
         self.funcs += [node.func]
         [self.visit(arg) for arg in node.args]
         [self.visit(kwarg) for kwarg in node.keywords]
-    
+
     def visit_Name(self, node):
         if hasattr(node, "attr"):
             self.deps += [f"{node.id}.{node.attr}"]
         else:
             self.deps += [node.id]
-    
+
     def report(self):
         pprint(self.deps)
 
-   
-# how can I know a ast.Name is a DataFrame column?
-for df_name, var_exprs in variables.items():
-    for expr in var_exprs:
-        #print(expr)
-        module = ast.parse(expr)
-        stmt = module.body[0]
-        lhs, = stmt.targets
-        rhs = stmt.value
-        analyzer = Analyzer()
-        analyzer.visit(stmt)
-        #analyzer.report()
-        dep_graph[f"{df_name}.{lhs.id}"] = [f"{dep}" if "." in dep 
-                                              else f"{df_name}.{dep}" 
-                                            for dep in analyzer.deps + 
-                                               analyzer.crosswalk.get("deps")
-                                           ]
-        defs[f"{df_name}.{lhs.id}"] = expr #rhs
+def analyze_dependency(vardef_yml):
+    dep_dict = {}
+    def_dict = {}
 
-        
-# tests
-def compare_dict(dict1, dict2):
-    for x1 in dict1.keys():
-        z = dict1.get(x1) == dict2.get(x1)
-        if not z:
-            print('key', x1)
-            print('Actual', dict1.get(x1), '\nExpected', dict2.get(x1))
-            print('-----\n')
-            
-expected_graph = {'household.nadults': ['household.hhsize', 'household.nchildren'],
- 'household.cars_per_adults': ['household.cars', 'household.nadults'],
- 'household.cars_per_adults_gt1': ['household.cars_per_adults'],
- 'household.sqrt_hhsize': ['household.hhsize'],
- 'household.nchildren': ['person.is_child', 'person.household_id'],
- 'person.is_child': ['person.age'],
- 'person.is_child_int': ['person.age'],                  
- 'person.nadults': ['household.nadults', 'person.household_id'],
- 'person.is_girl': ['person.is_child', 'person.sex']
-                 }
+    with open(vardef_yml) as file:
+        # The FullLoader parameter handles the conversion from YAML
+        # scalar values to Python the dictionary format
+        variables = yaml.load(file, Loader=yaml.FullLoader)
 
-compare_dict(dep_graph, expected_graph)
+        #print(variables)
+
+    # how can I know a ast.Name is a DataFrame column?
+    for df_name, var_exprs in variables.items():
+        for expr in var_exprs:
+            #print(expr)
+            module = ast.parse(expr)
+            stmt = module.body[0]
+            lhs, = stmt.targets
+            rhs = stmt.value
+            analyzer = DepAnalyzer()
+            analyzer.visit(stmt)
+            #analyzer.report()
+            dep_dict[f"{df_name}.{lhs.id}"] = [f"{dep}" if "." in dep
+                                                  else f"{df_name}.{dep}"
+                                                for dep in analyzer.deps +
+                                                   analyzer.crosswalk.get("deps")
+                                               ]
+            def_dict[f"{df_name}.{lhs.id}"] = expr #rhs
+    return dep_dict, def_dict
+
+
