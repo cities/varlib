85f0c44b2b838dea8a55c849805cf26a6dcc1f0c
lmwang <lmwang@gmail.com>
2019-11-28 12:10:05 -0800

    added more tests (disabled for now); refactored DepAnalyzer

--- tests/test_mod.py
+++ tests/test_mod.py
@@ -1,46 +1,110 @@
 from varlib.parse import build_graph, compute
 import pandas as pd
+import numpy as np
 
 # tests
 def compare_dict(dict1, dict2, verbose=True):
     for x1 in dict1.keys():
         z = dict1.get(x1) == dict2.get(x1)
-        assert z
         if not z and verbose:
             print('key', x1)
             print('Actual', dict1.get(x1), '\nExpected', dict2.get(x1))
             print('-----\n')
+        assert z
 
 
 #compare_dict(resulted_dict, expected_dict)
 def test_parse_deps():
-    expected_dict = {'household.nadults': ['household.hhsize', 'household.nchildren'],
+    expected_dict = {
+        'household.hhsize': ['person.person_id', 'person.household_id'],
+        'household.nadults': ['household.hhsize', 'household.nchildren'],
         'household.cars_per_adults': ['household.cars', 'household.nadults'],
         'household.cars_per_adults_gt1': ['household.cars_per_adults'],
         'household.sqrt_hhsize': ['household.hhsize'],
+        'household.log_hhsize': ['household.hhsize'],
         'household.nchildren': ['person.is_child', 'person.household_id'],
         'person.is_child': ['person.age'],
         'person.is_child_int': ['person.age'],
-        'person.nadults': ['household.nadults', 'person.household_id'],
-        'person.is_girl': ['person.is_child', 'person.sex']
+        'person.hh_nadults': ['household.nadults', 'person.household_id'],
+        'person.is_girl': ['person.is_child', 'person.sex'],
+        'zone.nadults': ['household.nadults', 'household.zone_id']
         }
 
     dep_graph = build_graph("example/variables.yml")
     resulted_dict = dict([(v, list(dep_graph.predecessors(v)))
-                        for v in expected_dict.keys()])
-    compare_dict(expected_dict, resulted_dict, verbose=False)
+                            for v in expected_dict.keys()])
+    compare_dict(resulted_dict, expected_dict, verbose=True)
 
-def test_compute():
-    dep_graph = build_graph("example/variables.yml")
 
+def prep_resolvers():
+    # Create DataFrame
     pp_data = {'person_id': [1, 2, 3, 4],
             'household_id': [1, 1, 2, 2],
             'age':[2,   26,  39, 10],
             'sex':['F', 'F', 'M', 'M']}
-    # Create DataFrame
     person = pd.DataFrame(pp_data).set_index("person_id")
     person.name = "person"
 
-    compute("person.is_girl", dep_graph, resolvers={"person":person})
+    hh_data = {'household_id': [1, 2],
+               'cars': [1, 2],
+               'zone_id': [1, 2]}
+    household = pd.DataFrame(hh_data).set_index("household_id")
+    household.name = "household"
+
+    zn_data = {'zone_id': [1, 2]}
+    zone = pd.DataFrame(zn_data).set_index("zone_id")
+    zone.name = "zone"
+    resolvers = {"person": person,
+                 "household": household,
+                 "zone": zone}
+    return resolvers
+
+dep_graph = build_graph("example/variables.yml")
+def test_compute_simple():
+    resolvers = prep_resolvers()
+    person = resolvers["person"]
+    compute("person.is_girl", dep_graph, resolvers=resolvers)
     assert 'is_girl' in person.columns
+    assert np.all(person['is_girl'].values ==
+                  np.array([True, False, False, False]))
     assert 'is_child' in person.columns
+    assert np.all(person['is_child'].values ==
+                  np.array([True, False, False, True]))
+    #compute("household.log_hhsize", dep_graph, resolvers=resolvers)
+    #assert 'log_hhsize' in household.columns
+    #assert np.all(household['log_hhsize'].values ==
+    #              np.array([True, False, False, True]))
+
+def DISABLED_test_compute_conversion():
+    resolvers = prep_resolvers()
+    compute("person.is_child_int", dep_graph, resolvers=resolvers)
+    compute("person.is_child_int2", dep_graph, resolvers=resolvers)
+    compute("person.is_child", dep_graph, resolvers=resolvers)
+    assert 'is_child' in person.columns
+    assert 'is_child_int' in person.columns
+    assert 'is_child_int2' in person.columns
+    assert person['is_child_int'].value == (person['is_child'].value).astype(int)
+    assert person['is_child_int'].value == person['is_child_int2']
+
+def DISABLED_test_compute_dep_other():
+    resolvers = prep_resolvers()
+    compute("household.cars_per_adults_gt1", dep_graph, resolvers=resolvers)
+    assert 'is_child' in person.columns
+    assert all(['nchildren', 'nadults', 'cars_per_adults'] in household.columns)
+    assert person['cars_per_adults_gt1'].value == pd.Series([])
+
+def DISABLED_test_compute_dep_others():
+    resolvers = prep_resolvers()
+    compute("zone.nadults", dep_graph, resolvers=resolvers)
+    assert 'is_child' in person.columns
+    assert all(['nchildren', 'nadults'] in household.columns)
+    assert 'nadults' in zone.columns
+    assert zone['nadults'].value == pd.Series([])
+
+def DISABLED_test_compute_round_trip():
+    resolvers = prep_resolvers()
+    compute("person.hh_nadults", dep_graph, resolvers=resolvers)
+    assert all(['is_child', 'hh_nadults'] in person.columns)
+    assert all(['nchildren', 'nadults'] in household.columns)
+    assert person['hh_nadults'].value == pd.Series([])
+
--- varlib/parse.py
+++ varlib/parse.py
@@ -5,47 +5,64 @@ import networkx as nx
 import pandas as pd
 from pprint import pprint
 
+_methods = [
+    'astype'
+]
+
 class DepAnalyzer(ast.NodeVisitor):
     def __init__(self):
         self.deps = []
         self.crosswalk = {"deps":[]}
         self.funcs = []
+        self.is_method = None
 
     def visit_Assign(self, node):
         lhs, = node.targets
         rhs = node.value
-        self.generic_visit(rhs)
+        self.visit(rhs)
 
     def visit_Attribute(self, node):
-        #print(ast.dump(node))
+        print(ast.dump(node))
         dataset = None
+        #import ipdb; ipdb.set_trace()
+        #maybe a dataset name
         if type(node.value) is ast.Name:
-            dataset = node.value.id
+            # node is an attribute, not a method
+            if node.attr not in _methods:
+                dataset = node.value.id
+            else:
+                self.deps +=[node.value.id]
         else:
             self.visit(node.value)
         if node.attr == "aggregate":
             #self.deps += [f"{node.value.id}.{node.value.id}_id"]
-            self.crosswalk["tgt_id"] = f"{dataset}_id"
+            self.crosswalk["tgt_ds"] = f"{dataset}"
         elif node.attr == "disaggregate":
             #self.deps += [f"{node.value.id}.{node.value.id}_id"]
             self.crosswalk["src_ds"] = f"{dataset}"
         elif dataset is not None:
             self.deps += [f"{dataset}.{node.attr}"]
-            if "tgt_id" in self.crosswalk:
-                self.crosswalk["deps"] += [f"{dataset}.{self.crosswalk['tgt_id']}"]
+            if "tgt_ds" in self.crosswalk:
+                self.crosswalk["deps"] += [f"{dataset}.{self.crosswalk['tgt_ds']}_id"]
             elif "src_ds" in self.crosswalk:
                 self.crosswalk["deps"] += [f"{self.crosswalk['src_ds']}.{dataset}_id"]
 
     def visit_Call(self, node):
+        #print(ast.dump(node))
         #if type(node.func) is ast.Name:
         #    self.generic_visit(node.args)
-        #if type(node.func) is ast.Attribute:
+        if type(node.func) is ast.Attribute:
+            self.is_method = True
         #    self.deps += [f"{node.func.value.id}.{node.func.value.id}_id"]
-        self.funcs += [node.func]
+        if type(node.func) is ast.Attribute:
+            self.visit(node.func)
+        else:
+            self.funcs += [node.func]
         [self.visit(arg) for arg in node.args]
         [self.visit(kwarg) for kwarg in node.keywords]
 
     def visit_Name(self, node):
+        print(ast.dump(node))
         if hasattr(node, "attr"):
             self.deps += [f"{node.id}.{node.attr}"]
         else:
@@ -88,7 +105,11 @@ def build_graph(vardef_yml, verbose=False):
     dep_graph = nx.DiGraph()
     for k, v in dep_dict.items():
         k_dep, k_expr, k_hash = v
-        dep_graph.add_node(k, expr=k_expr, hash=k_hash)
+        if not dep_graph.has_node(k):
+            dep_graph.add_node(k, expr=k_expr, hash=k_hash)
+        else:
+            dep_graph.nodes[k]['expr'] = k_expr
+            dep_graph.nodes[k]['hash'] = k_hash
         dep_graph.add_nodes_from(k_dep)
         edges = itertools.zip_longest(k_dep, [k], fillvalue=k)
         dep_graph.add_edges_from(edges)
@@ -99,7 +120,7 @@ def build_graph(vardef_yml, verbose=False):
     #return def_dict, dep_dict, dep_graph
     return dep_graph
 
-def compute(full_vname, dep_graph, resolvers):
+def compute(full_vname, dep_graph, resolvers, recompute=False):
     assert dep_graph.has_node(full_vname)
     df_name, vname= full_vname.split(".")
     df = resolvers[df_name]
